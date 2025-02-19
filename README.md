e050eadf0938d81caa92fc192c9ca372669d2c3d
M4TEU51-FECAF <mateus.silva.81788@a.fecaf.com.br>
2025-02-19 00:49:48 +0000

    aula_02

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # mateus.silva.4na
+
+Nome Completo: Mateus Pereira Silva
+RA: 81788
---
+++ aula_2/desafio_regressao_linear.py
@@ -0,0 +1,7 @@
+x = [1, 2, 3, 4, 5]
+y = [1, 2, 3, 4, 5]
+
+def regressao_linear(x,y):
+    print("Estou funcionando!")
+
+regressao_linear()
